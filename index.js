ccbdee6011ca952639a1123c0bc4e53b2b716cd0
Diana <160869579+fzxcvbn@users.noreply.github.com>
2024-05-24 23:31:59 +0300

    Более менее оптимизирован под комп

--- index.js
+++ index.js
@@ -38,22 +38,45 @@ let month61 = document.querySelector(".month61"),
     users2_forever = document.querySelector('.users2-forever'),
     users3_forever = document.querySelector('.users3-forever'),
     users4_forever = document.querySelector('.users4-forever'),
-    users15 = document.querySelector('.users15'),
-    users25 = document.querySelector('.users25'),
-    users35 = document.querySelector('.users35'),
-    users45 = document.querySelector('.users45'),
-    users110 = document.querySelector('.users110'),
-    users210 = document.querySelector('.users210'),
-    users310 = document.querySelector('.users310'),
-    users410 = document.querySelector('.users410'),
+    users115 = document.querySelector('.users115'),
     users125 = document.querySelector('.users125'),
+    users135 = document.querySelector('.users135'),
+    users145 = document.querySelector('.users145'),
+    users1110 = document.querySelector('.users1110'),
+    users1210 = document.querySelector('.users1210'),
+    users1310 = document.querySelector('.users1310'),
+    users1410 = document.querySelector('.users1410'),
+    users1125 = document.querySelector('.users1125'),
+    users1225 = document.querySelector('.users1225'),
+    users1325 = document.querySelector('.users1325'),
+    users1425 = document.querySelector('.users1425'),
+    users11100 = document.querySelector('.users11100'),
+    users12100 = document.querySelector('.users12100'),
+    users13100 = document.querySelector('.users13100'),
+    users14100 = document.querySelector('.users14100'),
+    users215 = document.querySelector('.users215'),
     users225 = document.querySelector('.users225'),
-    users325 = document.querySelector('.users325'),
-    users425 = document.querySelector('.users425'),
-    users1100 = document.querySelector('.users1100'),
-    users2100 = document.querySelector('.users2100'),
-    users3100 = document.querySelector('.users3100'),
-    users4100 = document.querySelector('.users4100')
+    users235 = document.querySelector('.users235'),
+    users245 = document.querySelector('.users245'),
+    users2110 = document.querySelector('.users2110'),
+    users2210 = document.querySelector('.users2210'),
+    users2310 = document.querySelector('.users2310'),
+    users2410 = document.querySelector('.users2410'),
+    users2125 = document.querySelector('.users2125'),
+    users2225 = document.querySelector('.users2225'),
+    users2325 = document.querySelector('.users2325'),
+    users2425 = document.querySelector('.users2425'),
+    users21100 = document.querySelector('.users21100'),
+    users22100 = document.querySelector('.users22100'),
+    users23100 = document.querySelector('.users23100'),
+    users24100 = document.querySelector('.users24100'),
+    free_btn_conteiner = document.querySelector('.free-btn-conteiner'),
+    erp_btn_conteiner = document.querySelector('.erp-btn-conteiner'),
+    conteiner2_2 = document.querySelector('.conteiner2-2'),
+    conteiner_free = document.querySelector('.conteiner-free'),
+    conteiner_erp = document.querySelector('.conteiner-erp'),
+    free_js = document.querySelector('.free-js'),
+    erp_free_js = document.querySelector('.erp-free-js')
 
 month61.addEventListener("click", function() {
     price1.style.color = 'black'
@@ -143,10 +166,10 @@ rent.addEventListener("click", function() {
     for_1_user_per_month2.textContent = 'за 1 пользователя в месяц при оплате за'
     for_1_user_per_month3.textContent = 'за 1 пользователя в месяц при оплате за'
     for_1_user_per_month4.textContent = 'за 1 пользователя в месяц при оплате за'
-    conteiner_month6_year1.style.display = 'block'
-    conteiner_month6_year2.style.display = 'block'
-    conteiner_month6_year3.style.display = 'block'
-    conteiner_month6_year4.style.display = 'block'
+    conteiner_month6_year1.style.display = 'flex'
+    conteiner_month6_year2.style.display = 'flex'
+    conteiner_month6_year3.style.display = 'flex'
+    conteiner_month6_year4.style.display = 'flex'
     users1_5_10_25_1001.style.display = 'none'
     users2_5_10_25_1001.style.display = 'none'
     users3_5_10_25_1001.style.display = 'none'
@@ -159,6 +182,7 @@ rent.addEventListener("click", function() {
     users2_forever.style.display = 'none'
     users3_forever.style.display = 'none'
     users4_forever.style.display = 'none'
+    free_btn_conteiner.style.display = 'block'
 })
 
 purchase.addEventListener("click", function() {
@@ -201,6 +225,15 @@ purchase.addEventListener("click", function() {
     users2_forever.style.display = 'block'
     users3_forever.style.display = 'block'
     users4_forever.style.display = 'block'
+    users1_forever.textContent = 'пользователей на навсегда'
+    users2_forever.textContent = 'пользователей на навсегда'
+    users3_forever.textContent = 'пользователей на навсегда'
+    users4_forever.textContent = 'пользователей на навсегда'
+    erp_btn_conteiner.style.display = 'none'
+    free_btn_conteiner.style.display = 'none'
+    conteiner_free.style.visibility = 'hidden'
+    conteiner_erp.style.visibility = 'visible'
+    erp_free_js.style.display = 'block'
 })
 
 subscription.addEventListener("click", function() {
@@ -243,132 +276,288 @@ subscription.addEventListener("click", function() {
     users2_forever.style.display = 'block'
     users3_forever.style.display = 'block'
     users4_forever.style.display = 'block'
+    users1_forever.textContent = 'пользователей на год'
+    users2_forever.textContent = 'пользователей на год'
+    users3_forever.textContent = 'пользователей на год'
+    users4_forever.textContent = 'пользователей на год'
+    erp_btn_conteiner.style.display = 'none'
+    free_btn_conteiner.style.display = 'none'
 })
 
-users15.addEventListener("click", function() {
-    users15.style.border = '1px solid black'
-    users110.style.border = 'none'
-    users125.style.border = 'none'
-    users1100.style.border = 'none'
+users115.addEventListener("click", function() {
+    users115.style.border = '1px solid black'
+    users1110.style.border = 'none'
+    users1125.style.border = 'none'
+    users11100.style.border = 'none'
     price1.textContent = '67 023 ₽'
-})
-
-users25.addEventListener("click", function() {
-    users25.style.border = '1px solid black'
-    users210.style.border = 'none'
-    users225.style.border = 'none'
-    users2100.style.border = 'none'
-    price2.textContent = '82 148 ₽'
-})
-
-users35.addEventListener("click", function() {
-    users35.style.border = '1px solid black'
-    users310.style.border = 'none'
-    users325.style.border = 'none'
-    users3100.style.border = 'none'
-    price3.textContent = '85 932 ₽'
-})
-
-users45.addEventListener("click", function() {
-    users45.style.border = '1px solid black'
-    users410.style.border = 'none'
-    users425.style.border = 'none'
-    users4100.style.border = 'none'
-    price4.textContent = '102 773 ₽'
-})
-
-users110.addEventListener("click", function() {
-    users110.style.border = '1px solid black'
-    users15.style.border = 'none'
-    users125.style.border = 'none'
-    users1100.style.border = 'none'
-    price1.textContent = '134 046 ₽'
-})
-
-users210.addEventListener("click", function() {
-    users210.style.border = '1px solid black'
-    users25.style.border = 'none'
-    users225.style.border = 'none'
-    users2100.style.border = 'none'
-    price2.textContent = '164 296 ₽'
-})
-
-users310.addEventListener("click", function() {
-    users310.style.border = '1px solid black'
-    users35.style.border = 'none'
-    users325.style.border = 'none'
-    users3100.style.border = 'none'
-    price3.textContent = '171 864 ₽'
-})
-
-users410.addEventListener("click", function() {
-    users410.style.border = '1px solid black'
-    users45.style.border = 'none'
-    users425.style.border = 'none'
-    users4100.style.border = 'none'
-    price4.textContent = '205 546 ₽'
 })
 
 users125.addEventListener("click", function() {
     users125.style.border = '1px solid black'
-    users15.style.border = 'none'
-    users110.style.border = 'none'
-    users1100.style.border = 'none'
+    users1210.style.border = 'none'
+    users1225.style.border = 'none'
+    users12100.style.border = 'none'
+    price2.textContent = '82 148 ₽'
+})
+
+users135.addEventListener("click", function() {
+    users135.style.border = '1px solid black'
+    users1310.style.border = 'none'
+    users1325.style.border = 'none'
+    users13100.style.border = 'none'
+    price3.textContent = '85 932 ₽'
+})
+
+users145.addEventListener("click", function() {
+    users145.style.border = '1px solid black'
+    users1410.style.border = 'none'
+    users1425.style.border = 'none'
+    users14100.style.border = 'none'
+    price4.textContent = '102 773 ₽'
+})
+
+users1110.addEventListener("click", function() {
+    users1110.style.border = '1px solid black'
+    users115.style.border = 'none'
+    users1125.style.border = 'none'
+    users11100.style.border = 'none'
+    price1.textContent = '134 046 ₽'
+})
+
+users1210.addEventListener("click", function() {
+    users1210.style.border = '1px solid black'
+    users125.style.border = 'none'
+    users1225.style.border = 'none'
+    users12100.style.border = 'none'
+    price2.textContent = '164 296 ₽'
+})
+
+users1310.addEventListener("click", function() {
+    users1310.style.border = '1px solid black'
+    users135.style.border = 'none'
+    users1325.style.border = 'none'
+    users13100.style.border = 'none'
+    price3.textContent = '171 864 ₽'
+})
+
+users1410.addEventListener("click", function() {
+    users1410.style.border = '1px solid black'
+    users145.style.border = 'none'
+    users1425.style.border = 'none'
+    users14100.style.border = 'none'
+    price4.textContent = '205 546 ₽'
+})
+
+users1125.addEventListener("click", function() {
+    users1125.style.border = '1px solid black'
+    users115.style.border = 'none'
+    users1110.style.border = 'none'
+    users11100.style.border = 'none'
     price1.textContent = '268 070 ₽'
+})
+
+users1225.addEventListener("click", function() {
+    users1225.style.border = '1px solid black'
+    users125.style.border = 'none'
+    users1210.style.border = 'none'
+    users12100.style.border = 'none'
+    price2.textContent = '328 570 ₽'
+})
+
+users1325.addEventListener("click", function() {
+    users1325.style.border = '1px solid black'
+    users135.style.border = 'none'
+    users1310.style.border = 'none'
+    users13100.style.border = 'none'
+    price3.textContent = '343 750 ₽'
+})
+
+users1425.addEventListener("click", function() {
+    users1425.style.border = '1px solid black'
+    users145.style.border = 'none'
+    users1410.style.border = 'none'
+    users14100.style.border = 'none'
+    price4.textContent = '411 070 ₽'
+})
+
+users11100.addEventListener("click", function() {
+    users11100.style.border = '1px solid black'
+    users115.style.border = 'none'
+    users1110.style.border = 'none'
+    users1125.style.border = 'none'
+    price1.textContent = '547 800 ₽'
+})
+
+users12100.addEventListener("click", function() {
+    users12100.style.border = '1px solid black'
+    users125.style.border = 'none'
+    users1210.style.border = 'none'
+    users1225.style.border = 'none'
+    price2.textContent = '687 500 ₽'
+})
+
+users13100.addEventListener("click", function() {
+    users13100.style.border = '1px solid black'
+    users135.style.border = 'none'
+    users1310.style.border = 'none'
+    users1325.style.border = 'none'
+    price3.textContent = '823 900 ₽'
+})
+
+users14100.addEventListener("click", function() {
+    users14100.style.border = '1px solid black'
+    users145.style.border = 'none'
+    users1410.style.border = 'none'
+    users1425.style.border = 'none'
+    price4.textContent = '1 372 800 ₽'
+})
+
+users215.addEventListener("click", function() {
+    users215.style.border = '1px solid black'
+    users2110.style.border = 'none'
+    users2125.style.border = 'none'
+    users21100.style.border = 'none'
+    price1.textContent = '33 511 ₽'
 })
 
 users225.addEventListener("click", function() {
     users225.style.border = '1px solid black'
-    users25.style.border = 'none'
-    users210.style.border = 'none'
-    users2100.style.border = 'none'
-    price2.textContent = '328 570 ₽'
+    users2210.style.border = 'none'
+    users2225.style.border = 'none'
+    users22100.style.border = 'none'
+    price2.textContent = '41 074 ₽'
 })
 
-users325.addEventListener("click", function() {
-    users325.style.border = '1px solid black'
-    users35.style.border = 'none'
-    users310.style.border = 'none'
-    users3100.style.border = 'none'
-    price3.textContent = '343 750 ₽'
+users235.addEventListener("click", function() {
+    users235.style.border = '1px solid black'
+    users2310.style.border = 'none'
+    users2325.style.border = 'none'
+    users23100.style.border = 'none'
+    price3.textContent = '42 966 ₽'
 })
 
-users425.addEventListener("click", function() {
-    users425.style.border = '1px solid black'
-    users45.style.border = 'none'
-    users410.style.border = 'none'
-    users4100.style.border = 'none'
-    price4.textContent = '411 070 ₽'
+users245.addEventListener("click", function() {
+    users245.style.border = '1px solid black'
+    users2410.style.border = 'none'
+    users2425.style.border = 'none'
+    users24100.style.border = 'none'
+    price4.textContent = '51 386 ₽'
 })
 
-users1100.addEventListener("click", function() {
-    users1100.style.border = '1px solid black'
-    users15.style.border = 'none'
-    users110.style.border = 'none'
-    users125.style.border = 'none'
-    price1.textContent = '547 800 ₽'
+users2110.addEventListener("click", function() {
+    users2110.style.border = '1px solid black'
+    users215.style.border = 'none'
+    users2125.style.border = 'none'
+    users21100.style.border = 'none'
+    price1.textContent = '67 023 ₽'
 })
 
-users2100.addEventListener("click", function() {
-    users2100.style.border = '1px solid black'
-    users25.style.border = 'none'
-    users210.style.border = 'none'
+users2210.addEventListener("click", function() {
+    users2210.style.border = '1px solid black'
     users225.style.border = 'none'
-    price2.textContent = '687 500 ₽'
+    users2225.style.border = 'none'
+    users22100.style.border = 'none'
+    price2.textContent = '82 148 ₽'
 })
 
-users3100.addEventListener("click", function() {
-    users3100.style.border = '1px solid black'
-    users35.style.border = 'none'
-    users310.style.border = 'none'
-    users325.style.border = 'none'
-    price3.textContent = '823 900 ₽'
+users2310.addEventListener("click", function() {
+    users2310.style.border = '1px solid black'
+    users235.style.border = 'none'
+    users2325.style.border = 'none'
+    users23100.style.border = 'none'
+    price3.textContent = '85 932 ₽'
 })
 
-users4100.addEventListener("click", function() {
-    users4100.style.border = '1px solid black'
-    users45.style.border = 'none'
-    users410.style.border = 'none'
-    users425.style.border = 'none'
-    price4.textContent = '1 372 800 ₽'
+users2410.addEventListener("click", function() {
+    users2410.style.border = '1px solid black'
+    users245.style.border = 'none'
+    users2425.style.border = 'none'
+    users24100.style.border = 'none'
+    price4.textContent = '102 773 ₽'
+})
+
+users2125.addEventListener("click", function() {
+    users2125.style.border = '1px solid black'
+    users215.style.border = 'none'
+    users2110.style.border = 'none'
+    users21100.style.border = 'none'
+    price1.textContent = '107 228 ₽'
+})
+
+users2225.addEventListener("click", function() {
+    users2225.style.border = '1px solid black'
+    users225.style.border = 'none'
+    users2210.style.border = 'none'
+    users22100.style.border = 'none'
+    price2.textContent = '131 428 ₽'
+})
+
+users2325.addEventListener("click", function() {
+    users2325.style.border = '1px solid black'
+    users235.style.border = 'none'
+    users2310.style.border = 'none'
+    users23100.style.border = 'none'
+    price3.textContent = '137 500 ₽'
+})
+
+users2425.addEventListener("click", function() {
+    users2425.style.border = '1px solid black'
+    users245.style.border = 'none'
+    users2410.style.border = 'none'
+    users24100.style.border = 'none'
+    price4.textContent = '164 428 ₽'
+})
+
+users21100.addEventListener("click", function() {
+    users21100.style.border = '1px solid black'
+    users215.style.border = 'none'
+    users2110.style.border = 'none'
+    users2125.style.border = 'none'
+    price1.textContent = '219 120 ₽'
+})
+
+users22100.addEventListener("click", function() {
+    users22100.style.border = '1px solid black'
+    users225.style.border = 'none'
+    users2210.style.border = 'none'
+    users2225.style.border = 'none'
+    price2.textContent = '275 000 ₽'
+})
+
+users23100.addEventListener("click", function() {
+    users23100.style.border = '1px solid black'
+    users235.style.border = 'none'
+    users2310.style.border = 'none'
+    users2325.style.border = 'none'
+    price3.textContent = '329 560 ₽'
+})
+
+users24100.addEventListener("click", function() {
+    users24100.style.border = '1px solid black'
+    users245.style.border = 'none'
+    users2410.style.border = 'none'
+    users2425.style.border = 'none'
+    price4.textContent = '549 120 ₽'
+})
+
+free_btn_conteiner.addEventListener("click", function() {
+    free_btn_conteiner.style.display = 'none'
+    conteiner2_2.style.left = '154px'
+    erp_btn_conteiner.style.display = 'block'
+    conteiner_free.style.visibility = 'visible'
+    conteiner_erp.style.visibility = 'hidden'
+    erp_free_js.style.display = 'none'
+    free_js.style.display = 'block'
+    crossed_out_price4.style.visibility = 'hidden'
+})
+
+erp_btn_conteiner.addEventListener("click", function() {
+    erp_btn_conteiner.style.display = 'none'
+    conteiner2_2.style.left = '-166px'
+    free_btn_conteiner.style.display = 'block'
+    conteiner_free.style.visibility = 'hidden'
+    conteiner_erp.style.visibility = 'visible'
+    conteiner2_2.style.transitionTimingFunction = 'linear'
+    free_js.style.display = 'none'
+    erp_free_js.style.display = 'block'
 })
